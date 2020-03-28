4d553257136dfb5f6f05e672de7bd15f5b4362db
Lucas Costa <lucascosta_s_a@hotmail.com>
2020-03-27 22:26:52 -0300

    criado o componente List

---
+++ listagem_front/src/components/list/list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './list.css'
+
+function list() {
+    return(
+        <main>
+            <ul>
+                <li className="task-item">
+
+                </li>
+            </ul>
+        </main>
+    )
+}
+
+export default List;
