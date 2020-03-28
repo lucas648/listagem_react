40acdff8f17a0686020b264e43e3ca0fb8760c55
Lucas Costa <lucascosta_s_a@hotmail.com>
2020-03-28 14:04:48 -0300

    adicionada dependencia cors

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,7 +12,7 @@ mongoose.connect('mongodb+srv://Lucas:lucasfiel@cluster0-xuva6.mongodb.net/proje
 });
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 app.listen(1803);
